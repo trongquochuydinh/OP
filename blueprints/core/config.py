9dc4858531a4c3ff6710a9c67d3a9d818764d935
trong <trong.dinh@inovec.technology>
2026-02-25 07:45:18 +0100

    chore: relocating config file

---
+++ blueprints/core/config.py
@@ -0,0 +1,9 @@
+CHART_TYPES= {
+    "boxplot": "box plot", 
+    "linechart": "line chart", 
+    "piechart": "pie chart", 
+    "barchart": "bar chart", 
+    "stackedbarchart": "stacked bar chart", 
+    "horizonvalclusteredbarchart": "horizontal clustered bar chart",
+    "dumbbellchart": "dumbbell chart"
+}
