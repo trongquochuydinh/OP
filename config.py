4728f34457029ed46e6b76acd43b3a2bffc85804
trong <trong.dinh@inovec.technology>
2026-02-25 07:43:56 +0100

    chore: project restructure

--- config.py
+++
@@ -1,9 +0,0 @@
-CHART_TYPES= {
-    "boxplot": "box plot", 
-    "linechart": "line chart", 
-    "piechart": "pie chart", 
-    "barchart": "bar chart", 
-    "stackedbarchart": "stacked bar chart", 
-    "horizonvalclusteredbarchart": "horizontal clustered bar chart",
-    "dumbbellchart": "dumbbell chart"
-}
